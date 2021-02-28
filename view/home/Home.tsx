c3d76d40375c93eda82fcc6e859e6a34a3027466
Marcos Chen <marcostcchen@gmail.com>
2021-02-28 15:14:44 -0300

    Beacon Tracker - WIP: Homepage first detection

--- view/home/Home.tsx
+++ view/home/Home.tsx
@@ -1,18 +1,55 @@
-import * as React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { PermissionsAndroid, Platform, SafeAreaView, StatusBar, Text } from 'react-native';
+import BleManager from 'react-native-ble-manager';
 
-export const HomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.centeredView}>
-      <Text>Home Screen</Text>
-    </SafeAreaView>
-  );
+interface Props {
+
 }
 
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+export const HomeScreen: React.FC<Props> = (props: Props) => {
+  const [isScanning, setIsScanning] = useState(false);
+
+  useEffect(() => {
+    BleManager.start({ showAlert: false });
+    getPermission();
+    startScan();
+  },[])
+
+  const startScan = () => {
+    if (!isScanning) {
+      BleManager.scan([], 3, true).then((results) => {
+        console.log('Scanning...');
+        setIsScanning(true);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
   }
-});
+
+  const getPermission = () => {
+    if (Platform.OS === 'android' && Platform.Version >= 23) {
+      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
+        if (result) {
+          console.log("Permission is OK");
+        } else {
+          PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
+            if (result) {
+              console.log("User accept");
+            } else {
+              console.log("User refuse");
+            }
+          });
+        }
+      });
+    }
+  }
+ 
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Aqui</Text>
+      </SafeAreaView>
+    </>
+  );
+}
