65f4c4dc81816786e1d0438d6148a70431dcef52
Marcos Chen <marcostcchen@gmail.com>
2021-08-06 16:48:45 -0300

    App - updated url with https

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
 // export const urlAPI = 'http://192.168.0.21/BeaconTrackerApi/api';
-export const urlAPI = 'http://www.beacontracker.software:2000/api';
+export const urlAPI = 'https://api.beacontracker.software/api';
